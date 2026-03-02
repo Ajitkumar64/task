2a7bff71dc0a880ddbf49e437d4392be69bb5c26
Ajit Kumar <ajitakumarbehera64@gmail.com>
2026-03-02 13:50:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+
+// IMPORTANT: use 0.0.0.0 instead of localhost
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Backend running on http://192.168.1.5:3000");
+});
